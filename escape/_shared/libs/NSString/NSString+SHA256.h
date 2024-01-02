721b29417691f2894e0dd659c41f3a3518a5da9d
Marcel <pwned4k@gmail.com>
2024-01-01 20:24:48 -0500

    removed binaries and added gzip extraction headers for a simpler process
    this should allow the app to be installed and not have issues when installed without using trollstore. This version should be compatible with altstore and other 3rd party app signers. opnly the app binary now needs to be re-signed. Like it should be. Also removed binpack.tar and changed it to a gz file, compressing the tar file from 90Mb to 30Mb 1/3 the size.

---
+++ escape/_shared/libs/NSString/NSString+SHA256.h
@@ -0,0 +1,15 @@
+//
+//  NSString+SHA256.h
+//  electra1131
+//
+//  Created by Pwn20wnd on 7/6/18.
+//  Copyright © 2018 Electra Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SHA256)
+
+- (NSString *)SHA256String;
+
+@end
